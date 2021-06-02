49e227f9792474cd4b41fc354cb8e7374d5b8220
Anthony-jcb <thonyjbc@gmail.com>
2021-06-01 23:52:22 -0430

    Bug fixes in database.sql and news.js

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -4,13 +4,7 @@ USE news_portal_nodejs;
 
 CREATE TABLE news(
     id_news INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tittle VARCHAR(100),
+    title VARCHAR(100),
     news TEXT,
     data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-DESCRIBE news;
-
-INSERT INTO news(tittle, news) values('My tittle', "contents of the news");
-
-SELECT * FROM news;
